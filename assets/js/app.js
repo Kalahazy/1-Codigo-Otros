dafeecf621b5a9c1c4772a9a8680ecce6617e309
Kalahazy <brandon_palacios11@hotmail.com>
2023-10-03 21:19:43 -0600

    feat: :sparkles: Code refactoring and correction, add comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
 const baseEndpoint = 'https://api.github.com';
+//Uso de backticks para poder hacer "String template" y así agregar al final de la variable baseEndpoint la dirección /users
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//Se cambia a . para indicar que la busqueda del querySelector es por clase y se quita el simbolo de $ para tener buena práctica en la declaración de variables
+const n = document.querySelector('.name');
+const b = document.querySelector('.blog'); 
+const l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+//Se agrega async a la funcion para poder usar el await
+async function displayUser(username) {
+  //Se quita el $ para referirnos a la variable n
+  n.textContent = 'cargando...';
+  //Se agregan backticks para poder hacer String template y concatenar la url de la variable usersEndpoint con el username recibido como parámetro
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //Se declara la variable "data" para almacenar la respuesta del fetch en formato JSON y se agrega un await para esperar a que responda el fetch
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //Se quita el $ de las variables y se cambian las comillas simples por backticks para poder hacer String template con las propiedades del objeto data (JSON)
+  n.textContent = `${data.name}`;
+  b.textContent = `${data.blog}`;
+  l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //Se agregan backticks para poder hacer String template con la variable err
+  n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
